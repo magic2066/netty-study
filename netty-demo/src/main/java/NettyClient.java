e6c1c8907827ea4f99ed16ed45fcca9f6d6942bc
penglei <penglei2066@gmail.com>
2023-03-19 14:58:02 +0800

    完成服务端启动流程

--- netty-demo/src/main/java/NettyClient.java
+++ netty-demo/src/main/java/NettyClient.java
@@ -2,30 +2,37 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 
 import java.util.Date;
 
 public class NettyClient {
-    public static void main(String[] args) throws InterruptedException {
-        Bootstrap bootstrap = new Bootstrap();
-        NioEventLoopGroup group = new NioEventLoopGroup();
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        bootstrap.group(group)
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                // 1.指定线程模型
+                .group(workerGroup)
+                // 2.指定 IO 类型为 NIO
                 .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<Channel>() {
+                // 3.IO 处理逻辑
+                .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(Channel ch) {
-                        ch.pipeline().addLast(new StringEncoder());
+                    public void initChannel(SocketChannel ch) {
                     }
                 });
+        // 4.建立连接
+        bootstrap.connect("juejin.cn", 80).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else {
+                System.err.println("连接失败!");
+            }
 
-        Channel channel = bootstrap.connect("2500o821w8.51vip.biz", 35453).channel();
-
-        while (true) {
-            channel.writeAndFlush(new Date() + ": hello world!");
-            Thread.sleep(2000);
-        }
+        });
     }
 }
--- netty-demo/src/main/java/NettyServer.java
+++ netty-demo/src/main/java/NettyServer.java
@@ -1,11 +1,14 @@
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 public class NettyServer {
     public static void main(String[] args) {
@@ -14,9 +17,9 @@ public class NettyServer {
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
         serverBootstrap
-                .group(boss, worker)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                .group(boss, worker) //配置线程组
+                .channel(NioServerSocketChannel.class) //指定IO模型 如果想指定为BIO：OioServerSocketChannel.class
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {  //定义后续每条连接的数据读写
                     protected void initChannel(NioSocketChannel ch) {
                         ch.pipeline().addLast(new StringDecoder());
                         ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
@@ -26,7 +29,34 @@ public class NettyServer {
                             }
                         });
                     }
-                })
-                .bind(8000);
+                });
+        bind(serverBootstrap, 8000);
+
+        //指定在服务端启动过程中的一些逻辑，通常情况下呢，我们用不着这个方法
+        serverBootstrap.handler(new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                System.out.println("服务端启动成功");
+            }
+        });
+
+        //表示系统用于临时存放已完成三次握手的请求的队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
+        serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+    }
+
+    /**
+     * 绑定端口，若端口被占用则自动+1 直至成功
+     * @param serverBootstrap 引导类
+     * @param port 端口
+     */
+    private static void bind(ServerBootstrap serverBootstrap, final int port){
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()){
+                System.out.println("端口[" + port + "]绑定成功");
+            }else {
+                System.out.println("端口[" + port + "]绑定绑定失败");
+                bind(serverBootstrap, port + 1);
+            }
+        });
     }
 }
